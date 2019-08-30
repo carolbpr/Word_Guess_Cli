477168da95170013aad7f4a5c6f2179590396123
Caro Berry <carolbpr@gmail.com>
2019-08-29 21:19:00 -0500

    almost fully working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const Word = require('./word.js');
+const color = require('colors/safe');
 var usastates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
     "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
     "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
@@ -7,7 +8,7 @@ var usastates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colo
 var wordselected = usastates[Math.floor(Math.random() * usastates.length)];
 var WordApp = new Word();
 var alredyguessedletter = [];
-var complete = false;
+
 var turns = 0;
 //var keyCode = event.which;
 function start() {
@@ -31,9 +32,11 @@ function enterAletter() {
         name: "UserGuess",
         message: "Guess a letter\n",
     }]).then(response => {
-
-        if (alredyguessedletter.includes(response.UserGuess) && complete === false) {
-            console.log("Choose another letter, you already guessed this\n");
+        console.log(alredyguessedletter);
+        if (alredyguessedletter.includes(response.UserGuess) && WordApp.complete === false) {
+            console.log(color.bgRed("Choose another letter, you already guessed this\n"));
+            console.log(WordApp.hiddenLetter.join(" "));
+            console.log("\n");
             enterAletter();
         }
         //else if ((keyCode < 65 || keyCode > 90) && complete === false) {
@@ -41,18 +44,39 @@ function enterAletter() {
         //}
 
         //if it is a new letter then these conditions will identify if the letter is part of the word or not
-        else if (complete === false) {
+        else if (WordApp.complete === false) {
             alredyguessedletter.push(response.UserGuess);
             WordApp.findtheLetter(response.UserGuess);
-            if (WordApp.lostAturn===true){turns++};
-            console.log(turns);
+            if (WordApp.lostAturn === true) { turns++ };
+
+            console.log(color.america("\nYou have " + (12 - turns) + " turns left\n"));
             if (turns === 12) {
-                console.log("Sorry! You lost. Try again !\n");
+                console.log(color.orange("Sorry! You lost. Try again !\n"));
                 start()
             }
             else enterAletter();
         }
+        if (WordApp.complete === true) {
+
+            console.log(color.blue("\nGreat Job! The Answer was " + WordApp.wordselected))
+            console.log(color.green("\nTry next word\n"));
+            nextWord();
+
+
+
+
+        }
 
     })
 
+};
+function nextWord() {
+    turns = 0;
+    alredyguessedletter = [];
+    WordApp.complete = false;
+    WordApp.newLetter = [];
+    WordApp.hiddenLetter = [];
+    wordselected = usastates[Math.floor(Math.random() * usastates.length)];
+    WordApp.underscoresFunction(wordselected);
+    enterAletter();
 }
--- word.js
+++ word.js
@@ -2,7 +2,7 @@
 const Letter = require('./letter.js');
 const color = require('colors/safe');
 function Word() {
-
+    this.complete = false;
     this.newLetter = [];
     this.hiddenLetter = [];
     this.underscoresFunction = (wordselected) => {
@@ -12,7 +12,8 @@ function Word() {
             this.hiddenLetter.push(this.newLetter[i].underscore());
         };
         console.log(this.hiddenLetter.join(" "));
-        console.log(this.newLetter);
+        console.log("\n");
+        //console.log(this.newLetter);
 
     }
     this.findtheLetter = (guessLetter) => {
@@ -20,27 +21,39 @@ function Word() {
         this.lostAturn = false;
         this.guessLetter = guessLetter;
         for (var i = 0; i < this.wordselected.length; i++) {
+            
             if (this.guessLetter.toUpperCase() === this.wordselected[i].toUpperCase()) {
-                this.lostAturn = false;
+                
                 this.hiddenLetter[i] = this.guessLetter.toUpperCase();
 
-            } else {
-            this.lettercount++;
-            this.lostAturn = true;
-               
+            } else if (this.guessLetter.toUpperCase() != this.wordselected[i].toUpperCase()){
+                this.lettercount++;
+                
+
             }
         }
-        console.log(this.lostAturn);
+        
         if (this.lettercount === this.wordselected.length) {
-            this.turn++
+            this.lostAturn = true;
             console.log(color.red("INCORRECT!!!\n"));
-        } else {
-            console.log("%cCORRECT!!!\n","color:green");
             console.log(this.hiddenLetter.join(" "));
             console.log("\n");
+        } else {
+            if (this.hiddenLetter.join("") === this.wordselected.toUpperCase()) {
+                this.complete = true;
+            }
+            else if (this.hiddenLetter.join("") != this.wordselected.toUpperCase()) {
+                this.lostAturn = false;
+                console.log(color.green("CORRECT!!!\n"));
+                console.log(this.hiddenLetter.join(" "));
+                console.log("\n");
+            }
         }
     }
-
 }
+
+
+
+
 
 module.exports = Word;
